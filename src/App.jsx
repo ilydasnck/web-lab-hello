34dca46999348088256671f02f54b18a30cc4590
İlayda Şenocak <isenocak229@gmail.com>
2026-02-24 23:06:43 +0300

    feat: add personal details to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>Web Tasarimi ve Programlama </h1>
+      <h2>LAB -1</h2>
+      <p>Ad Soyad: İlayda Şenocak</p>
+      <p>Ogrenci No: 230541004</p>
+      <p>Hobilerim: Keman çalmak, Kitap Okumak, Film İzlemek</p>
+      <p>
+        Ben İlayda Şenocak, 20 yaşındayım ve Elazığ'da yaşıyorum. Fırat
+        Üniversitesi Yazılım Mühendisliği bölümünde okuyorum.
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
